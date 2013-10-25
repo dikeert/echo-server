f950be16d409145e48a56ad7218d2314088a14a9
Anton Ermolenko <anton.ermolenko@outlook.com>
2013-10-25 18:41:24 +0400

    EventPool implementation started

--- src/main/java/com/ermys/echo/EventLoop.java
+++ src/main/java/com/ermys/echo/EventLoop.java
@@ -1,43 +1,34 @@
-/*
- This software is the confidential information and copyrighted work of
- NetCracker Technology Corp. ("NetCracker") and/or its suppliers and
- is only distributed under the terms of a separate license agreement
- with NetCracker.
- Use of the software is governed by the terms of the license agreement.
- Any use of this software not in accordance with the license agreement
- is expressly prohibited by law, and may result in severe civil
- and criminal penalties. 
- 
- Copyright (c) 1995-2013 NetCracker Technology Corp.
- 
- All Rights Reserved.
- 
-*/
 package com.ermys.echo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Singleton;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import static com.google.common.base.Throwables.propagate;
 
 /**
  * @author ANER0310
  *         Date: 10/25/13
  */
+@Singleton
 public class EventLoop {
+	private static final Logger log = LoggerFactory.getLogger(EventLoop.class);
+
+	private final BlockingQueue<EventDescriptor> descriptors = new LinkedBlockingQueue<EventDescriptor>();
+
+	public <T, J> void submit(Event<T> event, J sender){
+		descriptors.offer(new EventDescriptor<T, J>(event, sender));
+	}
+
+	public <T, J> EventDescriptor<T, J> take(){
+		try {
+			return descriptors.take();
+		} catch (InterruptedException e) {
+			log.error("Was interrupted", e);
+			throw propagate(e);
+		}
+	}
 }
-/*
- WITHOUT LIMITING THE FOREGOING, COPYING, REPRODUCTION, REDISTRIBUTION,
- REVERSE ENGINEERING, DISASSEMBLY, DECOMPILATION OR MODIFICATION
- OF THE SOFTWARE IS EXPRESSLY PROHIBITED, UNLESS SUCH COPYING,
- REPRODUCTION, REDISTRIBUTION, REVERSE ENGINEERING, DISASSEMBLY,
- DECOMPILATION OR MODIFICATION IS EXPRESSLY PERMITTED BY THE LICENSE
- AGREEMENT WITH NETCRACKER. 
- 
- THIS SOFTWARE IS WARRANTED, IF AT ALL, ONLY AS EXPRESSLY PROVIDED IN
- THE TERMS OF THE LICENSE AGREEMENT, EXCEPT AS WARRANTED IN THE
- LICENSE AGREEMENT, NETCRACKER HEREBY DISCLAIMS ALL WARRANTIES AND
- CONDITIONS WITH REGARD TO THE SOFTWARE, WHETHER EXPRESS, IMPLIED
- OR STATUTORY, INCLUDING WITHOUT LIMITATION ALL WARRANTIES AND
- CONDITIONS OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE,
- TITLE AND NON-INFRINGEMENT.
- 
- Copyright (c) 1995-2013 NetCracker Technology Corp.
- 
- All Rights Reserved.
-*/
